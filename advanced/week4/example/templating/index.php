3675e691616917137eca6a229bd06baf7111feb6
antwan <antwanvdm@gmail.com>
2019-12-09 16:53:15 +0100

    Example: templating

--- advanced/week4/example/templating/index.php
+++ advanced/week4/example/templating/index.php
@@ -1,17 +1,15 @@
 <?php require_once "includes/initialize.php"; ?>
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-    <title>Students Advanced PHP Module</title>
-    <meta name="description" content="Students Advanced PHP Module"/>
+    <title><?= $pageTitle ?? ''; ?></title>
+    <meta name="description" content="<?= $metaDescription ?? ''; ?>"/>
     <meta charset="utf-8"/>
 </head>
 <body>
 <?php if (isset($error)) : ?>
     <span class="error"><?= $error; ?></span>
 <?php endif; ?>
-<h1>Students Advanced PHP Module</h1>
-
-<div><?= $output; ?></div>
+<?= $content ?? ''; ?>
 </body>
 </html>
