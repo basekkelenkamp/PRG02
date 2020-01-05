d7a0c3a380d8b201a10ae4a940489ef80977e2b3
root <root@LAPTOP-DSR6CLO8.localdomain>
2020-01-05 20:05:32 +0100

    Initial commit

---
+++ CLE2/adminlogin.php
@@ -0,0 +1,30 @@
+<?php
+
+$password = "admin420";
+$username = "he";
+
+if($password == "admin420" && $username == "admin"){
+    echo "Access denied.";
+}
+else{
+    echo "Approved";
+}
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+    <link href="css/styles1.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
